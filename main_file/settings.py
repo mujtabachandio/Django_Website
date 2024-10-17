9dd256fe295edd541134a36b1e9c6b6781f8384a
mujtaba chandio <160246068+mujtabachandio@users.noreply.github.com>
2024-10-18 03:47:30 +0500

    updates

--- main_file/settings.py
+++ main_file/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-e0z(i#g4@73@5!ye4-u%b3pqvyqj9re4ht88g6bi^7e5pcx^)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
